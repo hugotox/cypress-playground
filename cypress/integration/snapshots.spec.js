d6b59786578b33126251bd3da35de2f78a2f0c5b
Hugo Pineda <hpineda83@gmail.com>
2020-10-18 19:20:24 -0700

    initial commit

---
+++ cypress/integration/snapshots.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Snapshots', () => {
+
+  it('works', () => {
+    cy.visit('https://www.zumper.com/apartment-buildings/p71295/1320-1380-lombard-russian-hill-san-francisco-ca')
+
+    // load all lazy sections by scrolling to the bottom:
+    cy.scrollTo('bottom', {duration: 1000})
+
+    // scroll back up
+    cy.scrollTo('top')
+
+    cy.matchImageSnapshot()
+  })
+
+})
